2aef5f91258d4d9319043e560088b991fcb394bd
DFqf <553111705@qq.com>
2026-09-17 17:19:30 +0800

    day01: setup Java environment and complete basic output exercises

---
+++ src/HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("姓名：闫仕玉");
+        System.out.println("学校：太原理工大学");
+        System.out.println("目标岗位：Java 后端开发 / AI 应用开发");
+
+        int a = 10;
+        int b = 3;
+
+        System.out.println("和：" + (a + b));
+        System.out.println("差：" + (a - b));
+        System.out.println("积：" + (a * b));
+        System.out.println("商：" + (a / b));
+    }
+}
