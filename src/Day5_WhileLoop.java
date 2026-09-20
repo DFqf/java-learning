4b2a35ff1a78a981310dcaa7f47ad1f779a7e011
DFqf <553111705@qq.com>
2026-09-20 11:35:10 +0800

    while循环

---
+++ src/Day5_WhileLoop.java
@@ -0,0 +1,26 @@
+public class Day5_WhileLoop {
+    public static void main(String[] args)
+    {
+
+        /*
+        int remainingDays=7;
+        while(remainingDays>0)
+        {
+            System.out.println("距离计划结束还有"+remainingDays+"天");
+            remainingDays--;
+        }
+
+        System.out.println("本周任务结束");
+        */
+
+        //用while输出10-1
+        int k=10;
+        while(k>=1)
+        {
+            System.out.println(k);
+            k--;
+        }
+
+
+    }
+}
